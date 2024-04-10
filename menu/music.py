0f079a275e92134825d6feacbe36ba213849259d
Kidojeager <kakashipro2111@gmail.com>
2024-04-10 10:46:51 +0700

    update sound 2

--- menu/music.py
+++ menu/music.py
@@ -4,14 +4,14 @@ from define import *
 
 class Music():
     def __init__(self, game):
-        self.game = game
         pygame.mixer.init()  # Initialize the mixer
         self.load_music()
         pygame.mixer.music.play()
 
+
     def load_music(self):
         pygame.mixer.music.load(PATH_SOUND + "8-bit.mp3")
-        self.set_volume(1)  # Set initial volume
+        self.set_volume(0.5)  # Set initial volume
 
     def set_volume(self, volume=None):
         if volume is not None:
@@ -19,13 +19,69 @@ class Music():
         pygame.mixer.music.set_volume(self.volume)
 
     def increase_volume(self):
-        self.volume = min(1.0, self.volume + 0.1)
+        self.volume = min(1.0, self.volume + 0.25)
         self.set_volume()
 
     def decrease_volume(self):
-        self.volume = max(0.0, self.volume - 0.1)
+        self.volume = max(0.0, self.volume - 0.25)
         self.set_volume()
 
     def get_volume(self):
         return self.volume
+class SoundEffect():
+    def __init__(self, game):
+        pygame.mixer.init()  # Initialize the mixer
+         # Set initial volume
+
+    def load_sound(self, sound_file):
+        self.set_volume(0.5)
+        return pygame.mixer.Sound(sound_file)
+
+    def play_sound(self, sound):
+        sound.set_volume(self.volume)
+        sound.play()
+
+    def stop_sound(self, sound):
+        sound.stop()
+
+    def set_volume(self, volume):
+        self.volume = volume
+
+    def increase_volume(self):
+        self.volume = min(1.0, self.volume + 0.25)
+
+    def decrease_volume(self):
+        self.volume = max(0.0, self.volume - 0.25)
+
+    def get_volume(self):
+        return self.volume
+
+
+"""
+if want to use
+from menu.music import *
+
+Example1: 
+In init:
+def __init__(self, game):
+self.sfx = SoundEffect(game)
+in event:
+elif event.key == pygame.K_t:  # T key
+                 ->  effect1 = self.sfx.load_sound(PATH_SOUND + "ahuevo.mp3")
+                -> self.sfx.play_sound(effect1)
+                 print("T key pressed")
+                 
+Example2:
+in init:
+def __init__(self, game):
+self.sfx = SoundEffect(game)
+        self.effect1 = self.sfx.load_sound(PATH_SOUND + "ahuevo.mp3")
+        
+        
+in event:
+                elif event.key == pygame.K_t:  # T key
+        ->            self.sfx.play_sound(self.effect1)
+                    print("T key pressed")
+
+"""
 
--- menu/options.py
+++ menu/options.py
@@ -1,6 +1,11 @@
+import sys
+
+import pygame
+
 from define import *
 from menu.menu import Menu
-from menu.music import Music
+from menu.music import *
+
 
 class CreditsMenu(Menu):
     def __init__(self, game):
@@ -25,67 +30,129 @@ class VolumeMenu(Menu):
         Menu.__init__(self, game)
         plus_btn_img = pygame.image.load(PATH_MENU + 'plus_btn.png').convert_alpha()
         minus_btn_img = pygame.image.load(PATH_MENU + 'minus_btn.png').convert_alpha()
-
+        self.red_bar_img = pygame.image.load(PATH_MENU + 'red_bar.png').convert_alpha()
         # Resize the images
-        resized_plus_btn_img = pygame.transform.scale(plus_btn_img, (24, 24))
-        resized_minus_btn_img = pygame.transform.scale(minus_btn_img, (24, 24))
-
+        resized_plus_btn_img = pygame.transform.scale(plus_btn_img, (40, 40))
+        resized_minus_btn_img = pygame.transform.scale(minus_btn_img, (40, 40))
         self.plus_btn_img = resized_plus_btn_img
         self.minus_btn_img = resized_minus_btn_img
-        self.btn_width, self.btn_height = self.plus_btn_img.get_width(), self.plus_btn_img.get_height()
-        self.plus_x, self.plus_y = PLUS_BTN_POSITION
-        self.minus_x, self.minus_y = MINUS_BTN_POSITION
         self.volume_bar_width, self.volume_bar_height = VOLUME_BAR_SIZE
-        self.volume_bar_position = VOLUME_BAR_POSITION
         self.filled_bar_width, self.filled_bar_height = FILLED_BAR_SIZE
-        self.filled_bar_position = FILLED_BAR_POSITION
+        self.game = game
         self.music = Music(game)
+        self.sfx = SoundEffect(game)
+        self.effect1 = self.sfx.load_sound(PATH_SOUND + "shot.wav")
+        self.selected_sound = "Music"
 
     def display_menu(self):
         self.run_display = True
+        music_text_y = WINDOW_HEIGHT / 4 + 120
+        sfx_text_y = WINDOW_HEIGHT / 4 + 230
         while self.run_display:
             self.game.check_events()
             if self.game.BACK_KEY:
                 self.game.curr_menu = self.game.options
                 self.run_display = False
             self.game.WINDOW_GAME.fill(COLOR_BACKGROUND)
-            self.game.draw_text('Volume', 20, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 4)
+            self.game.draw_text('Volume', 20, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 4 - 120)
+            self.game.draw_text('Music', 20, WINDOW_WIDTH / 2, music_text_y)
+            self.game.draw_text('Sfx', 20, WINDOW_WIDTH / 2, sfx_text_y)
+
+            if self.selected_sound == "Music":
+                self.game.draw_text('>', 20, WINDOW_WIDTH / 2 - 80, music_text_y)
+                self.game.draw_text('<', 20, WINDOW_WIDTH / 2 + 80, music_text_y)
+            elif self.selected_sound == "Sfx":
+                self.game.draw_text('>', 20, WINDOW_WIDTH / 2 - 80, sfx_text_y)
+                self.game.draw_text('<', 20, WINDOW_WIDTH / 2 + 80, sfx_text_y)
+            self.check_input()
             self.draw_volume_control()
-            self.handle_input()
             self.blit_screen()
 
 
     def draw_volume_control(self):
         # Draw minus button
-        self.game.WINDOW_GAME.blit(self.minus_btn_img, (self.minus_x, self.minus_y))
+        self.game.WINDOW_GAME.blit(self.minus_btn_img, (WINDOW_WIDTH / 2 - 160, WINDOW_HEIGHT / 3 + 80))
+        self.game.WINDOW_GAME.blit(self.minus_btn_img, (WINDOW_WIDTH / 2 - 160, WINDOW_HEIGHT / 3 + 210))
 
         # Draw plus button
-        self.game.WINDOW_GAME.blit(self.plus_btn_img, (self.plus_x, self.plus_y))
-
-        # Draw volume bar
-        pygame.draw.rect(self.game.WINDOW_GAME, COLOR_WHITE,
-                         (self.volume_bar_position[0], self.volume_bar_position[1],
-                          self.volume_bar_width, self.volume_bar_height), 2)
+        self.game.WINDOW_GAME.blit(self.plus_btn_img, (WINDOW_WIDTH / 2 + 120, WINDOW_HEIGHT / 3 + 80))
+        self.game.WINDOW_GAME.blit(self.plus_btn_img, (WINDOW_WIDTH / 2 + 120, WINDOW_HEIGHT / 3 + 210))
 
         # Calculate the width of the filled bar based on the volume level
-        filled_width = int(self.music.get_volume() * self.volume_bar_width - 4)
 
-        # Draw filled bar
+        music_width = int(self.music.get_volume() * self.volume_bar_width - 4)
+        music_width = max(0, min(music_width, self.volume_bar_width))
+        music_red_bar = pygame.transform.scale(self.red_bar_img, (music_width, self.filled_bar_height))
+
+        sfx_width = int(self.sfx.get_volume() * self.volume_bar_width - 4)
+        sfx_width = max(0, min(sfx_width, self.volume_bar_width))
+        sfx_red_bar = pygame.transform.scale(self.red_bar_img, (sfx_width, self.filled_bar_height))
+
+        pygame.draw.rect(self.game.WINDOW_GAME, COLOR_WHITE,
+                         (WINDOW_WIDTH / 2 - 102, WINDOW_HEIGHT / 3 + 80 - 2,
+                          self.volume_bar_width, self.volume_bar_height), 2)
+        self.game.WINDOW_GAME.blit(music_red_bar, (WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 3 + 80))
+
+        pygame.draw.rect(self.game.WINDOW_GAME, COLOR_WHITE,
+                         (WINDOW_WIDTH / 2 - 102, WINDOW_HEIGHT / 3 + 210 - 2,
+                          self.volume_bar_width, self.volume_bar_height), 2)
+        self.game.WINDOW_GAME.blit(sfx_red_bar, (WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 3 + 210))
+        """
+         # Draw volume bar & filled bar for music
+        pygame.draw.rect(self.game.WINDOW_GAME, COLOR_WHITE,
+                         (WINDOW_WIDTH / 2 - 102, WINDOW_HEIGHT / 3 + 80-2,
+                          self.volume_bar_width, self.volume_bar_height), 2)
         pygame.draw.rect(self.game.WINDOW_GAME, COLOR_RED,
-                         (self.filled_bar_position[0], self.filled_bar_position[1],
-                          filled_width, self.filled_bar_height))
+                         (WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 3 + 80,
+                          music_width, self.filled_bar_height))
 
-    def handle_input(self):
+        # Draw volume bar & filled bar for sfx
+        pygame.draw.rect(self.game.WINDOW_GAME, COLOR_WHITE,
+                         (WINDOW_WIDTH / 2 - 102, WINDOW_HEIGHT / 3 + 210-2,
+                          self.volume_bar_width, self.volume_bar_height), 2)
+        pygame.draw.rect(self.game.WINDOW_GAME, COLOR_RED,
+                         (WINDOW_WIDTH / 2 - 100, WINDOW_HEIGHT / 3 + 210,
+                          sfx_width, self.filled_bar_height))
+        """
+
+    def move_cursor(self):
+        if self.game.DOWN_KEY:
+            if self.selected_sound == 'Music':
+                self.selected_sound = 'Sfx'
+                print("Selected sound:", self.selected_sound)
+            elif self.selected_sound == 'Sfx':
+                self.selected_sound = 'Music'
+                print("Selected sound:", self.selected_sound)
+        elif self.game.UP_KEY:
+            if self.selected_sound == 'Music':
+                self.selected_sound = 'Sfx'
+                print("Selected sound:", self.selected_sound)
+            elif self.selected_sound == 'Sfx':
+                self.selected_sound = 'Music'
+                print("Selected sound:", self.selected_sound)
+    def check_input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.run_display = False
+                pygame.quit()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:  # Left arrow key
-                    self.music.decrease_volume()
-                    print("Left arrow key pressed")
-                elif event.key == pygame.K_RIGHT:  # Right arrow key
-                    self.music.increase_volume()
-                    print("Right arrow key pressed")
-
-
+                if event.key == pygame.K_LEFT:
+                    if self.selected_sound == "Music":
+                        self.music.decrease_volume()
+                    elif self.selected_sound == "Sfx":
+                        self.sfx.decrease_volume()
+                if event.key == pygame.K_RIGHT:
+                    if self.selected_sound == "Music":
+                        self.music.increase_volume()
+                    elif self.selected_sound == "Sfx":
+                        self.sfx.increase_volume()
+                if event.key == pygame.K_UP:
+                    self.selected_sound = "Music"
+                    print("Selected sound:", self.selected_sound)
+                if event.key == pygame.K_DOWN:
+                    self.selected_sound = "Sfx"
+                    print("Selected sound:", self.selected_sound)
+                if event.key == pygame.K_t:
+                    self.sfx.play_sound(self.effect1)
+                    print("T key pressed")
+        self.move_cursor()
     #TODO: Add volume control
